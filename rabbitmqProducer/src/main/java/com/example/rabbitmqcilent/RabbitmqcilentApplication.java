2761aa664bff903c6886c27a5c657de36a5450f1
Dalai426 <96421992+Dalai426@users.noreply.github.com>
2023-06-06 15:35:53 +0800

    Add files via upload

---
+++ rabbitmqProducer/src/main/java/com/example/rabbitmqcilent/RabbitmqcilentApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqcilent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqcilentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqcilentApplication.class, args);
+	}
+
+}
